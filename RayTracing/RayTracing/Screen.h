bbb368e34b7cfe51d2d98e25796bb30f679791f7
Arnaud DUHAMEL <a.duhamel@ateme.com>
2019-08-27 16:25:58 +0200

    screen info

--- RayTracing/RayTracing/Screen.h
+++ RayTracing/RayTracing/Screen.h
@@ -1,7 +1,17 @@
 #pragma once
 
+#include <QVector>
+#include "Point.h"
+
 class Screen
 {
+private:
+	QVector<int> m_u;
+	QVector<int> m_v;
+	int m_width;
+	int m_height;
+	Point m_position;
+
 public:
 	Screen();
 	virtual ~Screen();
