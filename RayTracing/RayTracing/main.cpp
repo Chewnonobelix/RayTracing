76edf806bf68a28a82ec224cfde35217664e4648
Arnaud DUHAMEL <chewnonobelix@gmail.com>
2019-08-28 22:04:41 +0200

    load and exec ray tracing

--- RayTracing/RayTracing/main.cpp
+++ RayTracing/RayTracing/main.cpp
@@ -1,8 +1,15 @@
 #include <QtCore/QCoreApplication>
+#include "Scene.h"
+#include "XmlLoader.h"
 
 int main(int argc, char *argv[])
 {
 	QCoreApplication a(argc, argv);
+	XmlLoader xml;
+	auto scene = xml.load("E:\\Dev\\RayTracing\\scene.xml");
 
+	scene.rayTracing();
+
+	qDebug() << "End";
 	return a.exec();
 }
